83eecff1023517cd805dee147bbfffe08a8aaa5f
Peter Quach <pquach1@icloud.com>
2023-11-03 11:34:38 -0400

    Phase 4

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -3,6 +3,11 @@ package com.pluralsight;
 public class Program {
 
     public static void main(String[] args) {
+        // Creating an instance of the UserInterfaceLayer class
+        UserInterface userInterface = new UserInterface();
 
+        // Call the display method to start the user interface
+        userInterface.display();
     }
 }
+
